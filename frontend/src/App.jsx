a8ea41a28f2f2c3d349d9a1780928a981b58fb61
Haksolot <sean.lamet.mail@gmail.com>
2025-06-10 11:16:39 +0200

    just pushing for estelle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="min-h-screen flex items-center justify-center">
+        <ul className="flex items-center justify-center gap-5 bg-gray-600 p-5 rounded-xl border-2 border-orange-600 shadow-md shadow-orange-500">
+          <li className="text-orange-500">
+            <svg
+              width="50"
+              height="49"
+              viewBox="0 0 50 49"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g filter="url(#filter0_d_21_7)">
+                <path
+                  d="M44.9895 20.4581C44.9895 21.8289 43.9476 22.9028 42.7667 22.9028H40.5439L40.5925 35.1031C40.5925 35.3087 40.5786 35.5144 40.5578 35.72V36.9537C40.5578 38.6368 39.3144 40 37.7793 40H36.6679C36.5915 40 36.5151 40 36.4387 39.9924C36.3414 40 36.2442 40 36.1469 40L33.8894 39.9924H32.2223C30.6872 39.9924 29.4438 38.6292 29.4438 36.9461V35.1183V30.2443C29.4438 28.8963 28.4505 27.8073 27.221 27.8073H22.7754C21.5459 27.8073 20.5526 28.8963 20.5526 30.2443V35.1183V36.9461C20.5526 38.6292 19.3093 39.9924 17.7741 39.9924H16.107H13.8912C13.787 39.9924 13.6828 39.9848 13.5786 39.9772C13.4953 39.9848 13.4119 39.9924 13.3285 39.9924H12.2171C10.682 39.9924 9.43865 38.6292 9.43865 36.9461V28.4165C9.43865 28.348 9.43865 28.2718 9.44559 28.2033V22.8951H7.2228C5.97247 22.8951 5 21.8289 5 20.4505C5 19.7651 5.20839 19.1558 5.69462 18.6227L23.4978 1.60926C23.9841 1.07616 24.5398 1 25.026 1C25.5122 1 26.0679 1.15231 26.4847 1.5331L44.2254 18.6303C44.7811 19.1634 45.059 19.7727 44.9895 20.4581Z"
+                  stroke="#FF6600"
+                  stroke-width="2"
+                  stroke-linejoin="round"
+                  shape-rendering="crispEdges"
+                />
+              </g>
+              <defs>
+                <filter
+                  id="filter0_d_21_7"
+                  x="0"
+                  y="0"
+                  width="50.0001"
+                  height="49"
+                  filterUnits="userSpaceOnUse"
+                  color-interpolation-filters="sRGB"
+                >
+                  <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                  <feColorMatrix
+                    in="SourceAlpha"
+                    type="matrix"
+                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                    result="hardAlpha"
+                  />
+                  <feOffset dy="4" />
+                  <feGaussianBlur stdDeviation="2" />
+                  <feComposite in2="hardAlpha" operator="out" />
+                  <feColorMatrix
+                    type="matrix"
+                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+                  />
+                  <feBlend
+                    mode="normal"
+                    in2="BackgroundImageFix"
+                    result="effect1_dropShadow_21_7"
+                  />
+                  <feBlend
+                    mode="normal"
+                    in="SourceGraphic"
+                    in2="effect1_dropShadow_21_7"
+                    result="shape"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </li>
+          <li className="text-orange-500">
+            <svg
+              width="31"
+              height="35"
+              viewBox="0 0 31 35"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M15.5 17.5C17.6975 17.5 19.805 16.6308 21.3589 15.0836C22.9128 13.5365 23.7857 11.438 23.7857 9.25C23.7857 7.06196 22.9128 4.96354 21.3589 3.41637C19.805 1.86919 17.6975 1 15.5 1C13.3025 1 11.195 1.86919 9.64111 3.41637C8.08724 4.96354 7.21429 7.06196 7.21429 9.25C7.21429 11.438 8.08724 13.5365 9.64111 15.0836C11.195 16.6308 13.3025 17.5 15.5 17.5ZM12.5417 20.5938C6.16563 20.5938 1 25.7371 1 32.0857C1 33.1428 1.86094 34 2.92254 34H28.0775C29.1391 34 30 33.1428 30 32.0857C30 25.7371 24.8344 20.5938 18.4583 20.5938H12.5417Z"
+                stroke="#FF6600"
+                stroke-width="2"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </li>
+          <li className="text-orange-500">
+            <svg
+              width="36"
+              height="35"
+              viewBox="0 0 36 35"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M5.25 1C2.90586 1 1 2.84988 1 5.12518V23.6885C1 25.9638 2.90586 27.8137 5.25 27.8137H11.625V32.9701C11.625 33.3633 11.8508 33.7178 12.2094 33.8918C12.568 34.0659 12.9996 34.0272 13.325 33.7952L21.5395 27.8137H30.75C33.0941 27.8137 35 25.9638 35 23.6885V5.12518C35 2.84988 33.0941 1 30.75 1H5.25Z"
+                stroke="#FF6600"
+                stroke-width="2"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </li>
+          <li className="text-orange-500">Notifications</li>
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
