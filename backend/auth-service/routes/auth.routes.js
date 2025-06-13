1378f397d682f4eb629701d854e60a552ffafde2
Estelle Laulhe <estelle.laulhe@gmail.com>
2025-06-13 12:04:35 +0200

    modif

--- backend/auth-service/routes/auth.routes.js
+++ backend/auth-service/routes/auth.routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const verifyToken = require("../middlewares/verifyToken");
+const verifyToken = require("../middlewares/verifyToken.middleware");
+const requiredFields = require("../middlewares/requiredFields.middleware");
 
 router.get("/", verifyToken, authController.getAllUsers);
-router.post("/register", authController.createUser);
-router.post("/login", authController.login);
+router.post(
+  "/register",
+  requiredFields(["pseudo", "email", "password", "role"]),
+  authController.createUser
+);
+router.post(
+  "/login",
+  requiredFields(["pseudo", "password"]),
+  authController.login
+);
 router.post("/authenticate", authController.authenticate);
 
 module.exports = router;
